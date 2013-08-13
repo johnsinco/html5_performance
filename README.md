8937c532b6a0f2c2533ee25df0a0a58c35fdada4
johnsinco <lead.j2ee.engineer@gmail.com>
2013-08-13 10:59:29 -0600

    stats js

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ html5_performance
 =================
 
 A simple javascript library for recording browser performance statistics using the html5 performance API
+
+Simply download and include browser_events.js in your javascript assets, and ...
+
+based upon http://www.html5rocks.com/en/tutorials/webperformance/basics/
+
+ see also:  http://calendar.perfplanet.com/2011/a-practical-guide-to-the-navigation-timing-api/
---
+++ html5_performance_stats.js
@@ -0,0 +1,132 @@
+// utility to log browser timing and report-back to a host server
+// based upon http://www.html5rocks.com/en/tutorials/webperformance/basics/
+// see also:  http://calendar.perfplanet.com/2011/a-practical-guide-to-the-navigation-timing-api/
+// author: John Stewart @john_s_in_co
+
+var s;
+var BrowserEvents = {
+
+  settings: {
+    dataSendUrl:  "/browser_events"
+  },
+
+  init: function() {
+    s = this.settings;
+    return this;
+  },
+
+  config: function(settings) {
+    for(var att in this.settings) {
+      if(settings[att] !== undefined) {
+        this.settings[att] = settings[att];
+      }
+    }
+    return this.settings;
+  },
+
+  // log the timing of the current page to the local storage
+  logTiming: function() {
+    // get the browser performance data and save to local storage
+    try {
+      if(BrowserEvents.supports_performance_stats() && BrowserEvents.supports_html5_storage()) {
+        stats = BrowserEvents.getPerfStats();
+        data = localStorage['perfTiming'] || "[]";
+        data = JSON.parse(data);
+        data.push(stats);
+        localStorage['perfTiming'] = JSON.stringify(data);
+        // if the data count > 10, send to the server
+        if(data.length > 10) {
+          BrowserEvents.sendToServer();
+        }
+      }
+    } catch(e) {
+      return false;
+    }
+  },
+
+  // log an error to local storage
+  logError: function(error) {
+    if(BrowserEvents.supports_html5_storage()) {
+      errors = localStorage['errors'] || "[]";
+      errors = JSON.parse(errors);
+      errors.push(error);
+      localStorage['errors'] = JSON.stringify(errors);
+      error;
+    }
+  },
+
+  // send all local event data to the server
+  sendToServer: function() {
+    console.log("sending performance stats to host");
+    try {
+      var req = BrowserEvents.getAjax();
+      statString = localStorage['perfTiming'];
+      if(req != null && statString != null) {
+        req.open('POST', s['dataSendUrl'], true);
+        req.setRequestHeader("Content-type","application/json");
+        req.send(statString);
+        localStorage.removeItem('perfTiming');
+      }
+    } catch(e) {
+      console.log(e);
+      return false;
+    }
+  },
+
+  getPerfStats: function() {
+    if(BrowserEvents.supports_performance_stats()) {
+      var timing = window.performance.timing;
+      var nav = window.performance.navigation;
+      return {
+        path: window.location.pathname,
+        search: window.location.search, 
+        dns: timing.domainLookupEnd - timing.domainLookupStart,
+        connect: timing.connectEnd - timing.connectStart,
+        basePage: timing.responseEnd - timing.responseStart,
+        frontEnd: timing.loadEventStart - timing.responseEnd,
+        networkLatency: timing.responseEnd - timing.fetchStart,
+        fullLoad: timing.loadEventEnd - timing.navigationStart,
+        requestTime: timing.navigationStart,
+        redirects: nav.redirectCount
+      };
+    } else {
+      return null;
+    }
+  },
+
+  supports_html5_storage: function() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+      return false;
+    }
+  }, 
+
+  supports_performance_stats: function() {
+    try {
+      return 'performance' in window && window['performance'] !== null && window['performance']['timing'] !== null
+    } catch (e) {
+      return false;
+    }
+  },
+
+  getAjax: function() {
+    if (window.XMLHttpRequest) {
+        return new window.XMLHttpRequest;
+    } else {
+      try {
+        return new ActiveXObject("MSXML2.XMLHTTP");
+      } catch (ex) {
+        return null;
+      }
+    }
+  }
+
+};
+
+window.onload = function(){
+  BrowserEvents.init();
+  setTimeout(function(){
+    BrowserEvents.logTiming();
+  }, 2000);
+};
